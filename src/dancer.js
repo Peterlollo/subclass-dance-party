2257de094a6fde64fc56aeb016eb191e8d475dd1
peterlollo <peteralollo@gmail.com>
2016-01-18 15:15:01 -0800

    Implement pseudoclassical version of Dancer class

--- src/dancer.js
+++ src/dancer.js
@@ -30,3 +30,31 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   return dancer;
 };
+
+
+var Dancer = function(top, left, timeBetweenSteps) {
+  this.$node = $('<span class="dancer"></span>');
+  this.left = left;
+  this.top = top;
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.step();
+  this.setPosition(top, left);
+};
+
+Dancer.prototype.step = function () {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step, this.timeBetweenSteps);
+};
+
+Dancer.prototype.setPosition = function(top, left) {
+    // Use css top and left properties to position our <span> tag
+    // where it belongs on the page. See http://api.jquery.com/css/
+    //
+    var styleSettings = {
+      top: top,
+      left: left
+    };
+    this.$node.css(styleSettings);
+};
+
